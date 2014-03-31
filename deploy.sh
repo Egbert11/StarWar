12450bf01491a4390050808ef7492bd07c05480e
gzwufulin <gzwufulin@corp.netease.com>
2014-03-31 10:36:21 +0800

    增加json测试数据和发布脚本

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cp -r css /home/nijun/work/webservice/dailyactive/htdocs
+cp -r image /home/nijun/work/webservice/dailyactive/htdocs
+cp -r js /home/nijun/work/webservice/dailyactive/htdocs
+cp index.html /home/nijun/work/webservice/dailyactive/htdocs
